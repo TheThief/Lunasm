bfc72e8767b19cd8ca1f9b6e076b922f0a659c33
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-02-10 10:38:27 -0300

    refactore: fixed some spaces

--- configure.sh
+++ configure.sh
@@ -3,9 +3,9 @@
 CHECK_FOLDERS='build, bin'
 
 for folder in $(echo $CHECK_FOLDERS | sed "s/,/ /g"); do
-	if [[ ! -d $folder ]]; then
-  	echo -e "Creating $folder..."; mkdir $folder
-	fi
+  if [[ ! -d $folder ]]; then
+    echo -e "Creating $folder..."; mkdir $folder
+  fi
 done
 
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -S . -B build/
