1e42b40595df1bbba4e411f8735cb247b69b9888
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-02-12 13:00:22 -0300

    refactore(main): added the lexer to test the tokenizer function

--- src/main.cpp
+++ src/main.cpp
@@ -8,70 +8,7 @@
 #include "fmt/core.h"
 #include "cxxopts.hpp"
 
-namespace Lunasm {
-
-  class Lexer 
-  {
-  public:
-    Lexer(const std::string& source_code)
-      : m_sourceCode(source_code)
-    {}
-    
-    char current() const
-    {
-      if (m_index >= m_sourceCode.length())
-        return '\0';
-
-      return m_sourceCode.at(m_index);
-    }
-
-    void advance()
-    {
-      m_index++;
-    }
-
-    char eat()
-    {
-      return m_sourceCode.at(m_index++);
-    }
-
-    std::optional<char> peek(std::size_t pos)
-    {
-      if (m_index + pos < m_sourceCode.size())
-        return m_sourceCode.at(m_index + pos);
-
-      return {};
-    }
-
-    void tokenizer() 
-    {
-      while (m_index < m_sourceCode.length())
-      {
-        if (std::isdigit(current()))
-        {
-          std::size_t start = m_index;
-
-          advance();
-
-          while (std::isdigit(current()) || current() == '.') 
-            advance();
-
-          std::size_t end = m_index - start;
-          std::string text = m_sourceCode.substr(start, end);
-
-          fmt::print("Token: {}\n", std::stof(text.c_str()));
-        }
-
-        if (current() == ' ')
-          advance();
-        }
-    }
-
-  private:
-    std::size_t m_index{0};
-    std::string m_sourceCode{0};
-  };
-}
+#include "include/Lexer/Lexer.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -88,6 +25,9 @@ int main(int argc, char* argv[])
     fmt::print("{}", options.help());
     std::exit(0);
   }
+
+  auto l = Lunasm::Lexer("ldi r1, $01");
+  l.Tokenizer();
 
   return EXIT_SUCCESS;
 }
