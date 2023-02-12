5c3bba5401448d3645b65d08e19b2c8df9ed620c
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-02-12 16:19:32 -0300

    refactore(lexer): added the void parameter to the right functions
    also added a line to the lexer

--- src/include/Lexer/Lexer.hpp
+++ src/include/Lexer/Lexer.hpp
@@ -9,15 +9,16 @@ namespace Lunasm {
 	public:
 		Lexer(const std::string& source_code);
 	public:
-		char eat();
+		char eat(void);
 		void step(void);
 		void skip(std::size_t n);
-		bool is_empty() const ;
+		bool is_empty(void) const;
 		char current_char(void) const;
 		std::optional<char> peek(std::size_t pos);
 	public:
 		void Tokenizer();
 	private:
+		std::size_t m_line{1};
 		std::size_t m_index{0};
 		std::string m_source_code{0};
 	};
